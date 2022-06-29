ce277c3260a725f1b8bc7876478b2a9a891a2745
charlotte-divine <divinemaina@gmail.com>
2022-06-29 23:06:52 +0100

    fix linter errors manually

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,58 +1,58 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-function Calculator () {
-  const [state,setState] = useState({obj:{
-    total: null,
-    next: null,
-    operation: null,
-  }})
+function Calculator() {
+  const [state, setState] = useState({
+    obj: {
+      total: null,
+      next: null,
+      operation: null,
+    },
+  });
 
-  const handleClick = (buttonName)=>setState(({obj})=>({obj:calculate(obj,buttonName)}));
+  const handleClick = (buttonName) => setState(({ obj }) => ({ obj: calculate(obj, buttonName) }));
 
   return (
     <div className="calculator-main-cont">
-      
+
       <div className="calculator-output">{state.obj.next || state.obj.total || 0}</div>
-        <div className="calculator-wrapper">
-          <div className="calculator-numbers-cont">
-            <div className="calculator-numbers">
-              <button onClick={()=>handleClick("AC")}>AC</button>
-              <button onClick={()=>handleClick("+/-")}>+/-</button>
-              <button onClick={()=>handleClick('%')}>%</button>
-            </div>
-            <div className="calculator-numbers">
-              <button onClick={()=>handleClick('7')}>7</button>
-              <button onClick={()=>handleClick('8')}>8</button>
-              <button onClick={()=>handleClick('9')}>9</button>
-            </div>
-            <div className="calculator-numbers">
-              <button onClick={()=>handleClick('4')}>4</button>
-              <button onClick={()=>handleClick('5')}>5</button>
-              <button onClick={()=>handleClick('6')}>6</button>
-            </div>
-            <div className="calculator-numbers">
-              <button  onClick={()=>handleClick("1")}>1</button>
-              <button  onClick={()=>handleClick("2")}>2</button>
-              <button  onClick={()=>handleClick('3')}>3</button>
-            </div>
-            <div className="calculator-numbers">
-              <button onClick={()=>handleClick('0')}>0</button>
-              <button onClick={()=>handleClick('.')}>.</button>
-            </div>
+      <div className="calculator-wrapper">
+        <div className="calculator-numbers-cont">
+          <div className="calculator-numbers">
+            <button type="button" onClick={() => handleClick('AC')}>AC</button>
+            <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
+            <button type="button" onClick={() => handleClick('%')}>%</button>
           </div>
-          <div className="calculator-symbol-cont">
-            <button onClick={()=>handleClick('÷')}>÷</button>
-            <button onClick={()=>handleClick('x')}>×</button>
-            <button onClick={()=>handleClick('-')}>-</button>
-            <button onClick={()=>handleClick("+")}>+</button>
-            <button onClick={()=>handleClick("=")}>=</button>
+          <div className="calculator-numbers">
+            <button type="button" onClick={() => handleClick('7')}>7</button>
+            <button type="button" onClick={() => handleClick('8')}>8</button>
+            <button type="button" onClick={() => handleClick('9')}>9</button>
+          </div>
+          <div className="calculator-numbers">
+            <button type="button" onClick={() => handleClick('4')}>4</button>
+            <button type="button" onClick={() => handleClick('5')}>5</button>
+            <button type="button" onClick={() => handleClick('6')}>6</button>
+          </div>
+          <div className="calculator-numbers">
+            <button type="button" onClick={() => handleClick('1')}>1</button>
+            <button type="button" onClick={() => handleClick('2')}>2</button>
+            <button type="button" onClick={() => handleClick('3')}>3</button>
+          </div>
+          <div className="calculator-numbers">
+            <button type="button" onClick={() => handleClick('0')}>0</button>
+            <button type="button" onClick={() => handleClick('.')}>.</button>
           </div>
         </div>
+        <div className="calculator-symbol-cont">
+          <button type="button" onClick={() => handleClick('÷')}>÷</button>
+          <button type="button" onClick={() => handleClick('x')}>×</button>
+          <button type="button" onClick={() => handleClick('-')}>-</button>
+          <button type="button" onClick={() => handleClick('+')}>+</button>
+          <button type="button" onClick={() => handleClick('=')}>=</button>
+        </div>
       </div>
-    );
-  }
-
+    </div>
+  );
+}
 
 export default Calculator;

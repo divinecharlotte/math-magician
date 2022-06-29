8eeae6dd2348f43ebd5f82b1d6af31e676460ee6
charlotte-divine <divinemaina@gmail.com>
2022-06-29 14:27:56 +0100

    add calculator events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,51 +1,61 @@
+/* eslint-disable */
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+     obj: {
+        total: null,
+        next: null,
+        operation: null,
+      }
+    };
   }
+
+handleClick = (buttonName)=>this.setState(({obj})=>({obj:calculate(obj,buttonName)}));
 
   render() {
     return (
       <div className="calculator-main-cont">
-        <div className="calculator-output">0</div>
+        
+        <div className="calculator-output">{this.state.obj.next || this.state.obj.total || 0}</div>
         <div className="calculator-wrapper">
           <div className="calculator-numbers-cont">
             <div className="calculator-numbers">
-              <b>AC</b>
-              <b>+/-</b>
-              <b>%</b>
+              <button onClick={()=>this.handleClick("AC")}>AC</button>
+              <button onClick={()=>this.handleClick("+/-")}>+/-</button>
+              <button onClick={()=>this.handleClick('%')}>%</button>
             </div>
             <div className="calculator-numbers">
-              <b>7</b>
-              <b>8</b>
-              <b>9</b>
+              <button onClick={()=>this.handleClick('7')}>7</button>
+              <button onClick={()=>this.handleClick('8')}>8</button>
+              <button onClick={()=>this.handleClick('9')}>9</button>
             </div>
             <div className="calculator-numbers">
-              <b>4</b>
-              <b>5</b>
-              <b>6</b>
+              <button onClick={()=>this.handleClick('4')}>4</button>
+              <button onClick={()=>this.handleClick('5')}>5</button>
+              <button onClick={()=>this.handleClick('6')}>6</button>
             </div>
             <div className="calculator-numbers">
-              <b>1</b>
-              <b>2</b>
-              <b>3</b>
+              <button  onClick={()=>this.handleClick("1")}>1</button>
+              <button  onClick={()=>this.handleClick("2")}>2</button>
+              <button onClick={()=>this.handleClick('3')}>3</button>
             </div>
             <div className="calculator-numbers">
-              <b>0</b>
-              <b>.</b>
+              <button onClick={()=>this.handleClick('0')}>0</button>
+              <button onClick={()=>this.handleClick('.')}>.</button>
             </div>
           </div>
           <div className="calculator-symbol-cont">
-            <b>÷</b>
-            <b>×</b>
-            <b>-</b>
-            <b>+</b>
-            <b>=</b>
+            <button onClick={()=>this.handleClick('÷')}>÷</button>
+            <button onClick={()=>this.handleClick('x')}>×</button>
+            <button onClick={()=>this.handleClick('-')}>-</button>
+            <button  onClick={()=>this.handleClick("+")}>+</button>
+            <button  onClick={()=>this.handleClick("=")}>=</button>
           </div>
         </div>
-
       </div>
     );
   }
